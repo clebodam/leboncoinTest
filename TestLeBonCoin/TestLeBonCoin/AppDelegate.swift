e94fb9b85efeee5a5a5ffed5353f45d930b25e7d
damien <dfe@connecthings.com>
2020-09-22 16:18:59 +0200

    add xcode project

---
+++ TestLeBonCoin/TestLeBonCoin/AppDelegate.swift
@@ -0,0 +1,67 @@
+//
+//  AppDelegate.swift
+//  TestLeBonCoin
+//
+//  Created by Damien on 22/09/2020.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        if #available(iOS 13.0, *) {
+            // In iOS 13 setup is done in SceneDelegate
+        } else {
+            self.window?.makeKeyAndVisible()
+        }
+
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+        // Not called under iOS 13 - See SceneDelegate sceneWillResignActive
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        // Not called under iOS 13 - See SceneDelegate sceneDidEnterBackground
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        // Not called under iOS 13 - See SceneDelegate sceneWillEnterForeground
+        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        // Not called under iOS 13 - See SceneDelegate sceneDidBecomeActive
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    }
+
+    func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+        // Override point for customization after application launch.
+        if #available(iOS 13.0, *) {
+            // In iOS 13 setup is done in SceneDelegate
+        } else {
+            let window = UIWindow(frame: UIScreen.main.bounds)
+            self.window = window
+
+            let viewController = ViewController()
+            window.rootViewController = viewController
+        }
+
+        return true
+    }
+
+
+
+
+}
+
