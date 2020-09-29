c38f87bb1fb6209c58cd6ada305eca3e0be2d31b
damien <dfe@connecthings.com>
2020-09-29 09:47:05 +0200

    reorganize filter call

--- TestLeBonCoin/TestLeBonCoin/Screens/List/UI/ListTableViewController.swift
+++ TestLeBonCoin/TestLeBonCoin/Screens/List/UI/ListTableViewController.swift
@@ -22,8 +22,17 @@ class ListTableViewController: UITableViewController {
     }
     // MARK: - UI SETUP
     override func viewDidLoad() {
-
         super.viewDidLoad()
+        setupUI()
+        bindViewModel()
+        reloadAction()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        navigationController?.navigationBar.barTintColor = .white
+    }
+
+    func setupUI() {
         self.view.backgroundColor = .white
         // set accessibilityIdentifier for testUI
         tableView.accessibilityIdentifier = "tableView"
@@ -50,14 +59,6 @@ class ListTableViewController: UITableViewController {
         self.tableView.rowHeight = UITableView.automaticDimension
         self.tableView.estimatedRowHeight = 150
         self.tableView.separatorStyle = .none
-        bindViewModel()
-        reloadAction()
-        setNeedsStatusBarAppearanceUpdate()
-    }
-
-    override func viewWillAppear(_ animated: Bool) {
-        navigationController?.navigationBar.barTintColor = .white
-
     }
 
     func bindViewModel() {
@@ -81,12 +82,10 @@ class ListTableViewController: UITableViewController {
     // MARK: - TableView delegation
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
         return viewModel.filteredItems.value.count
     }
 
@@ -98,7 +97,6 @@ class ListTableViewController: UITableViewController {
                 itemCell.viewModel = itemViewModel
             }
         }
-
         return cell
     }
 
@@ -122,52 +120,7 @@ class ListTableViewController: UITableViewController {
     }
     
     @objc func filterAction(sender: UIButton) {
-        let alert = UIAlertController(title: NSLocalizedString("filter_title", comment: ""),
-                                      message: nil,
-                                      preferredStyle: UIAlertController.Style.actionSheet)
-        alert.isModalInPopover = true
-        var pickerViewValues: [[String]] = [[String]]()
-        if let values = viewModel.getCategories()?.map({$0.name }) {
-            pickerViewValues = [values]
-        }
-        var selectedIndex = 0
-        if let selectedCategory =  self.viewModel.filterCategory {
-            selectedIndex =  self.viewModel.getCategories()?.firstIndex(of:  selectedCategory) ?? 0
-        }
-
-        let pickerViewSelectedValue: PickerViewViewController.Index = (column: 0, row: selectedIndex)
-
-        alert.addPickerView(values: pickerViewValues, initialSelection: pickerViewSelectedValue) { vc, picker, index, values in
-            let categories =  self.viewModel.getCategories()
-            self.viewModel.filterCategory = categories?[index.row]
-        }
-
-        let okAction = UIAlertAction(title: NSLocalizedString("ok_button", comment: ""), style: .default, handler: {
-            (alert: UIAlertAction!) -> Void in
-            if  self.viewModel.filterCategory == nil {
-                let categories =  self.viewModel.getCategories()
-                if let cat = categories?[0] {
-                    self.viewModel.filterCategory = cat
-                }
-            }
-            self.viewModel.filter()
-        })
-
-        alert.addAction(okAction)
-        let cancelAction = UIAlertAction(title: NSLocalizedString("cancel_button", comment: ""), style: .destructive, handler: { _ in
-            self.viewModel.filterCategory = nil
-            self.viewModel.filter()
-        })
-        alert.addAction(cancelAction)
-
-        if let presenter = alert.popoverPresentationController {
-            presenter.barButtonItem =  UIBarButtonItem(customView: sender)
-        }
-        // here we have an issue Will attempt to recover by breaking constraint
-        //<NSLayoutConstraint:0x600001823390 UIView:0x7fb277d68230.width == - 16   (active)>
-        // this a known bug  https://stackoverflow.com/questions/55372093/uialertcontrollers-actionsheet-gives-constraint-error-on-ios-12-2-12-3
-
-        self.parent?.present(alert, animated: true)
+        self.viewModel.presentFilter(on: self, sender: sender)
     }
 }
 
